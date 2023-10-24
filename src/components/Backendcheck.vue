8956a94900fcfc5159c89ef7ca8817cca17e52c5
sam cross <sam.cross@gmx.com>
2023-10-24 10:43:12 +0000

    added vite and api stuff

---
+++ src/components/Backendcheck.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const text = ref('not loaded');
+
+onMounted(async () => {
+    const env = await import.meta.env;
+  try {
+    const response = await axios.get(`${env.VITE_APP_API_BASE_URL}/api/ping`);
+    text.value = response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+</script>
+
+<template>
+  <div>
+    <hr>
+    <h1>{{ text }}</h1>
+    <hr>
+  </div>
+</template>
